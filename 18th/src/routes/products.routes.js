7b08d9ec92a46cff2be7dcce78b48646fefda50a
NicolasLRX <nicolaslrx@gmail.com>
2024-09-21 20:30:16 -0300

    Entrega 18

---
+++ 18th/src/routes/products.routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import productController from "../controllers/products.controller.js";
+import {
+  authorization,
+  passportCall,
+} from "../middlewares/passport.middleware.js";
+import { productDataValidartor } from "../validators/productData.validator.js";
+const router = Router();
+
+//solicitudes / peticiones
+
+router.get("/", productController.getAll);
+router.post(
+  "/",
+  passportCall("jwt"),
+  authorization("admin"),
+  productDataValidartor,
+  productController.create
+);
+router.get("/:pid", productController.getById);
+router.put(
+  "/:pid",
+  passportCall("jwt"),
+  authorization("admin"),
+  productController.update
+);
+router.delete(
+  "/:pid",
+  passportCall("jwt"),
+  authorization("admin"),
+  productController.del
+);
+
+export default router;
